832137e8d732afd69bafd2b838c36cfc9c0aea6b
harshika53 <rathod6155@gmail.com>
2025-05-19 20:30:23 +0530

    added minor changes

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -15,7 +15,7 @@ function Home2() {
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
+            <h1 style={{ fontSize: "2.3em" }}>
               GET <span className="purple"> KNOW MORE  </span> ABOUT ME 
             </h1>
             <p className="home-about-body" style={{ textAlign: "justify" }}>
