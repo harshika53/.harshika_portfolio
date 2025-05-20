75e1fc586e1df199e9bceec420449a96fd835015
harshika53 <rathod6155@gmail.com>
2025-05-20 16:30:19 +0530

    added two aboutcards

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import LeetCodeStats from "./LeetCode";
 import Techstack from "./Techstack";
-import Aboutcard from "./AboutCard";
+import AboutCard from "./AboutCard";
 import Toolstack from "./Toolstack";
 
 function About() {
@@ -16,35 +16,44 @@ function About() {
             md={7}
             style={{
               justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
+              paddingTop: "20px",
+              paddingBottom: "20px",
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+              My <strong className="purple"> Campus Involvement </strong> and <strong className="purple">  Leadership</strong>
             </h1>
-            <Aboutcard />
+            <AboutCard type="involvement" />
           </Col>
-          {/*<Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
+        </Row>
+
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "20px",
+              paddingBottom: "50px",
+            }}
           >
-           <img src={laptopImg} alt="about" className="img-fluid" />
-          </Col>*/}
+            <h1 style={{ fontSize: "2.1em", padding: "10px 0 20px" }}>
+              <strong className="purple">Certifications</strong> I've Completed
+             </h1>
+            <AboutCard type="certifications" />
+          </Col>
         </Row>
 
         <h1 className="project-heading">
           Technologies <strong className="purple">I Use</strong>
         </h1>
-
         <Techstack />
 
         <h1 className="project-heading">
           <strong className="purple">Tools</strong> I use
         </h1>
         <Toolstack />
-            <LeetCodeStats />
+
+        <LeetCodeStats />
       </Container>
     </Container>
   );
