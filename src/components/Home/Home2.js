da9868d69e1bee4d057b334831cf7e3e06f40974
harshika53 <rathod6155@gmail.com>
2025-05-20 10:56:41 +0530

    made minor changes

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,7 +16,7 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.3em" }}>
-              GET <span className="purple"> KNOW MORE  </span> ABOUT ME 
+              KNOW WHO <span className="purple"> I'M  </span>
             </h1>
             <p className="home-about-body" style={{ textAlign: "justify" }}>
                I really enjoy web development — learning new stuff, trying out different tools, and building cool things is what keeps me going.I’ve got a strong interest in <b className="purple">software development </b> and <b className="purple">cloud computing </b> — especially working with <b className="purple"> AWS </b> and I’m always looking for opportunities to contribute to impactful projects in these areas.
@@ -25,7 +25,7 @@ function Home2() {
               Lately, I’ve also started exploring cybersecurity a bit, especially through CTF events and hands-on learning. It’s something I’m curious about and plan to dive deeper into.
               <br />
               <br />
-               These days, I’m focused on improving my skills in <b className="purple"> React.js </b> and other modern <b className="purple"> Javascript frameworks </b> I regularly practice <b className="purple"> DSA </b> on  <b className="purple"> LeetCode </b> and <b className="purple"> HackerRank </b> to get better at problem-solving, and I’ve taken part in hackathons like <b className="purple"> Smart India Hackathon </b> to gain practical experience.
+               These days, I’m focused on improving my skills in <b className="purple"> React.js & Python </b> and other modern <b className="purple"> Javascript frameworks </b> I regularly practice <b className="purple"> DSA </b> on  <b className="purple"> LeetCode </b> and <b className="purple"> HackerRank </b> to get better at problem-solving, and I’ve taken part in hackathons like <b className="purple"> Smart India Hackathon </b> to gain practical experience.
                <br />
                <br />
                My goal is to work on meaningful projects in a dynamic and collaborative environment where I can grow, learn, and help the organization succeed through my contributions.
